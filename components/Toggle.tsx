07735e98bd43c71cc83a3a4b2b95eabb016b0ead
Nicole Massaro <nicole@massaro.ca>
2021-10-31 15:28:55 -0400

    feat: add toggle component

---
+++ components/Toggle.tsx
@@ -0,0 +1,25 @@
+import { Switch } from '@headlessui/react'
+import { classNames } from '@utils/classNames'
+
+type ToggleProps = {
+  enabled: boolean
+  setEnabled: (isEnabled: boolean) => void
+  label: string
+}
+
+export const Toggle = ({ enabled, setEnabled, label }: ToggleProps) => {
+  return (
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className={classNames(enabled ? 'bg-green-600' : 'bg-gray-300',
+        'relative inline-flex items-center h-6 rounded-full w-11 transition-all ease-in-out')}
+    >
+      <span className="sr-only">{label}</span>
+      <span
+        className={`${enabled ? 'translate-x-6' : 'translate-x-1'
+          } inline-block w-4 h-4 transform bg-white rounded-full transition-transform ease-in-out`}
+      />
+    </Switch>
+  )
+}
